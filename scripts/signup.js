8d62b5c753a746347af30feafa1b18be4f685d95
Eliascolin <eliascolina.c@gmail.com>
2023-06-13 21:06:53 -0300

    colocando name as tag input de signup.html colocando  minlengt  e maxlength aos input,  chamando as tag no signup.js

---
+++ scripts/signup.js
@@ -0,0 +1,22 @@
+
+                         //seleccionando o input nome
+const nameInputRef= document.querySelector("#nameInput")
+
+                         // seleccionando o input Sobrenome
+const lastNameInputRef= document.querySelector("#lastNameInput")
+
+                         // seleccionando o input
+const emailInputRef= document.querySelector("#emailInput")
+
+                         // seleccionando o input senha
+const passwordInputRef= document.querySelector("#passwordInput")
+
+                          // seleccionando o input repetir senha
+const passwordRepitInputRef = document.querySelector("#passwordRepitInput")
+
+                         // seleccionando o botao
+const buttonFormRef = document.querySelector("#buttonForm")
+
+
+
+
