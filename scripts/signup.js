b94348746d84349e5dd6c85d62814e887617c7ba
Eliascolin <eliascolina.c@gmail.com>
2023-06-14 22:39:48 -0300

    criando as funçoes de erro de senha

--- scripts/signup.js
+++ scripts/signup.js
@@ -13,6 +13,10 @@ const passwordInputRef= document.querySelector("#passwordInput")
 
                           // seleccionando o input repetir senha
 const passwordRepitInputRef = document.querySelector("#passwordRepitInput")
+                               
+                          //mensagem de erro
+const mensagemErro = document.querySelector(".erro_mensagem");
+
 
                          // seleccionando o botao
 const buttonFormRef = document.querySelector("#buttonForm")
@@ -30,17 +34,88 @@ let formaHasError = {
 
 
 
+// validar senha
+
+function passwordValid(event){
+
+
+
+    
+}
+
+
+/// avaliar os imput
+function validarInput(event){
+    const target = event.target
+    const isValid = target.checkValidity()
+
+
+
+    if(isValid){
+        //adicionar clase de error
+            target.classList.remove("error")
+            formaHasError[target.name] = false
+            mensagemErro.textContent = ""
+
+
+            // coferir se repetir senha é igual a senha 
+            if(target.name ==='passwordRepit' ){
+               
+                if(passwordInputRef.value !== passwordRepitInputRef.value){
+              
+                    mensagemErro.textContent = "senha invalida";
+                    formaHasError[target.name] = true
+                    target.classList.add("error")
+                }else{target.classList.remove("error")
+                formaHasError[target.name] = false
+                mensagemErro.textContent = ""}
+            }else if(target.name ==='password'){
+
+                if(passwordRepitInputRef.value !== passwordInputRef.value){
+              
+                    mensagemErro.textContent = "senha invalida";
+                    formaHasError[target.name] = true
+                    target.classList.add("error")
+                }else{target.classList.remove("error")
+                formaHasError[target.name] = false
+                mensagemErro.textContent = ""} 
+
+            }
+
+             
+        }
+        
+        
+        
+        else{
+        
+          //mensagem de erro no small
+          mensagemErro.textContent = "Valor inválido";
+        
+          // caso seja invalido formaHasError para true a propiedade eque estiver com erro
+          formaHasError[target.name] = true
+          target.classList.add("error")
+            
+        }
+
+
+        disabledButtonErro()
+
+}  
+
+
+
 //deshabilitar botao
 function disabledButtonErro(){
   
 
     if(!formaHasError.name && !formaHasError.lastName && !formaHasError.email && !formaHasError.password && !formaHasError.passwordRepit ){
     
-      botonForm.disabled = false
+        buttonFormRef.disabled = false
        
     }else{
     
-      botonForm.disabled = true
+        buttonFormRef.disabled = true
       
     }
     
@@ -48,3 +123,22 @@ function disabledButtonErro(){
     
     
     }
+
+
+
+                                  //evento ao clicar no nome
+    nameInputRef.addEventListener("keyup",(event)=>validarInput(event))
+
+                                  // evento ao clicar n sobrenome
+    lastNameInputRef.addEventListener("keyup",(event)=>validarInput(event))
+
+                                  // evento ao clicar no email
+ emailInputRef.addEventListener("keyup",(event)=>validarInput(event))
+                                  
+
+
+                              //evento ao clicar na senha    
+    passwordInputRef.addEventListener("input",(event)=>validarInput(event))
+
+                               //evento ao clicar na repetir  senha  
+    passwordRepitInputRef.addEventListener("input",(event)=>validarInput(event))
