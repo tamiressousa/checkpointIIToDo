e62233396a1b61b8079859b3f9e63e15f4519c7f
Eliascolin <eliascolina.c@gmail.com>
2023-06-19 20:48:12 -0300

    resolvendo bug do botao  em signup.js e signup.html

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,24 +1,24 @@
 
-                         //seleccionando o input nome
-const nameInputRef= document.querySelector("#nameInput")
+//seleccionando o input nome
+const nameInputRef = document.querySelector("#nameInput")
 
-                         // seleccionando o input Sobrenome
-const lastNameInputRef= document.querySelector("#lastNameInput")
+// seleccionando o input Sobrenome
+const lastNameInputRef = document.querySelector("#lastNameInput")
 
-                         // seleccionando o input
-const emailInputRef= document.querySelector("#emailInput")
+// seleccionando o input
+const emailInputRef = document.querySelector("#emailInput")
 
-                         // seleccionando o input senha
-const passwordInputRef= document.querySelector("#passwordInput")
+// seleccionando o input senha
+const passwordInputRef = document.querySelector("#passwordInput")
 
-                          // seleccionando o input repetir senha
+// seleccionando o input repetir senha
 const passwordRepitInputRef = document.querySelector("#passwordRepitInput")
-                               
-                          //mensagem de erro
+
+//mensagem de erro
 const mensagemErro = document.querySelector(".erro_mensagem");
 
 
-                         // seleccionando o botao
+// seleccionando o botao
 const buttonFormRef = document.querySelector("#buttonForm")
 
 
@@ -27,124 +27,129 @@ const buttonFormRef = document.querySelector("#buttonForm")
 
 //coferir se os input estao com erro 
 let formaHasError = {
-    name: true,
-    lastName: true,
-    email: true,
-    password: true,
-    passwordRepit: true
-  }
+  name: true,
+  lastName: true,
+  email: true,
+  password: true,
+  passwordRepit: true
+}
 
 
 /// avaliar os imput
-function validarInput(event){
-    const target = event.target
-    const isValid = target.checkValidity()
+function validarInput(event) {
+  const target = event.target
+  const isValid = target.checkValidity()
 
 
 
-    if(isValid){
-        //adicionar clase de error
-            target.classList.remove("error")
-            formaHasError[target.name] = true
-            mensagemErro.textContent = ""
+  if (isValid) {
+    //adicionar clase de error
+    target.classList.remove("error")
+    formaHasError[target.name] = false
+    mensagemErro.textContent = ""
 
 
-            // coferir se repetir senha é igual a senha 
-            if(target.name ==='passwordRepit' ){
-               
-                if(passwordInputRef.value !== passwordRepitInputRef.value){
-              
-                    mensagemErro.textContent = "senha diferente";
-                    formaHasError[target.name] = true
-                    formaHasError.password = true
-                    passwordRepitInputRef.classList.add("error")
-                }else{passwordRepitInputRef.classList.remove("error")
-                formaHasError[target.name] = true
-                mensagemErro.textContent = ""}
-            }else if(target.name ==='password'){
+    // coferir se repetir senha é igual a senha 
+    if (target.name === 'passwordRepit') {
 
-                if(passwordRepitInputRef.value !== passwordInputRef.value){
-              
-                    mensagemErro.textContent = "senha diferente";
-                    formaHasError[target.name] = false
-                    passwordRepitInputRef.classList.add("error")
-                }else{target.classList.remove("error")
-                formaHasError[target.name] = true
-                mensagemErro.textContent = ""} 
+      if (passwordInputRef.value !== passwordRepitInputRef.value) {
 
-            }
+        mensagemErro.textContent = "senha diferente";
+        formaHasError[target.name] = true
+        formaHasError.password = true
+        passwordRepitInputRef.classList.add("error")
+      } else {
+        passwordRepitInputRef.classList.remove("error")
+        formaHasError[target.name] = false
+        formaHasError.password=false
+        mensagemErro.textContent = ""
+      }
+    } else if (target.name === 'password') {
 
-             
-        }
-        
-        
-        
-        else{
-        
-          //mensagem de erro no small
-          mensagemErro.textContent = "Valor inválido";
-        
-          // caso seja invalido formaHasError para true a propiedade eque estiver com erro
-          formaHasError[target.name] = false
-          target.classList.add("error")
-            
-        }
+      if (passwordRepitInputRef.value !== passwordInputRef.value) {
+
+        mensagemErro.textContent = "senha diferente";
+        formaHasError[target.name] = true
+        passwordRepitInputRef.classList.add("error")
+      } else {
+        target.classList.remove("error")
+        formaHasError[target.name] = false
+        mensagemErro.textContent = ""
+      }
+
+    }
 
 
-        disabledButtonErro()
+  }
 
-        console.log(formaHasError)
 
-}  
+
+  else {
+
+    //mensagem de erro no small
+    mensagemErro.textContent = "Valor inválido";
+
+    // caso seja invalido formaHasError para true a propiedade eque estiver com erro
+    formaHasError[target.name] = true
+    target.classList.add("error")
+
+  }
+
+
+  disabledButtonErro()
+
+  console.log(formaHasError)
+
+}
 
 
 
 //deshabilitar botao
 function disabledButtonErro() {
-    if (
-      !formaHasError.name &&
-      !formaHasError.lastName &&
-      !formaHasError.email &&
-      !formaHasError.password &&
-      !formaHasError.passwordRepit
-    ) {
-      buttonFormRef.disabled = false;
-    } else {
-      buttonFormRef.disabled = true;
-    }
+  if (
+    !formaHasError.name &&
+    !formaHasError.lastName &&
+    !formaHasError.email &&
+    !formaHasError.password &&
+    !formaHasError.passwordRepit
+  ) {
+    buttonFormRef.disabled = false;
+  } else {
+    buttonFormRef.disabled = true;
   }
+}
 
 
 
-         // funçao do botao
-function buttonSent(){
+// funçao do botao
+function buttonSent() {
 
 
-    window.location.href ="../pages/tarefas.html"
+  window.location.href = "../pages/tarefas.html"
 
 
 }
 
 
-                                  //evento ao clicar no nome
-    nameInputRef.addEventListener("keyup",(event)=>validarInput(event))
+//evento ao clicar no nome
+nameInputRef.addEventListener("keyup", (event) => validarInput(event))
 
-                                  // evento ao clicar n sobrenome
-    lastNameInputRef.addEventListener("keyup",(event)=>validarInput(event))
+// evento ao clicar n sobrenome
+lastNameInputRef.addEventListener("keyup", (event) => validarInput(event))
 
-                                  // evento ao clicar no email
- emailInputRef.addEventListener("keyup",(event)=>validarInput(event))
-                                  
+// evento ao clicar no email
+emailInputRef.addEventListener("keyup", (event) => validarInput(event))
 
 
-                              //evento ao clicar na senha    
-    passwordInputRef.addEventListener("keyup",(event)=>validarInput(event))
 
-                               //evento ao clicar na repetir  senha  
-    passwordRepitInputRef.addEventListener("keyup",(event)=>validarInput(event))
+//evento ao clicar na senha    
+passwordInputRef.addEventListener("keyup", (event) => validarInput(event))
 
-                                 //evento ao clicar no botao
-buttonFormRef.addEventListener("click",(event)=> {
-   event.preventDefault()
-    buttonSent()
+//evento ao clicar na repetir  senha  
+passwordRepitInputRef.addEventListener("keyup", (event) => validarInput(event))
+
+//evento ao clicar no botao
+buttonFormRef.addEventListener("click", (event) => {
+  event.preventDefault()
+  buttonSent()
 });
